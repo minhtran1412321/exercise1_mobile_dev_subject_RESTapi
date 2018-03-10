3e5f48eecb9022e418127ac59362fd11278fab2d
Minh Tran <tqminh3011@gmail.com>
2018-03-11 01:43:51 +0700

    fixing

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ app.use(bodyParser.json());
 
 app.use(require('./controllers'));
 
-app.listen('3000', function(){
+app.listen(process.env.PORT || 3000, function(){
 	console.log('service is running');
 });
